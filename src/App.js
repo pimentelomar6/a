43d725e9b12c4672eb1d28cfa1ac6c2c7da83c73
Omar Pimentel <pimentelomar6@gmail.com>
2023-03-07 01:39:55 -0400

    Agregando componente NotFound

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Trailer from "./components/trailer/Trailer";
 import Reviews from "./components/reviews/Reviews";
+import NotFound from "./components/notfound/NotFound";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -58,6 +59,7 @@ function App() {
               />
             }
           ></Route>
+          <Route path="*" element={<NotFound/>}></Route>
         </Route>
       </Routes>
     </div>
---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'
+
+
+const NotFound = () => {
+    return(
+        <div className='not-found-text'>
+            Esta página no puede ser encontrada.
+        </div>
+    )
+}
+
+export default NotFound
