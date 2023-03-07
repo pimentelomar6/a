374b94def97276f4a1398a263fa9354e309f0d60
Omar Pimentel <pimentelomar6@gmail.com>
2023-03-07 01:09:29 -0400

    first commit

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api-movies-production-2544.up.railway.app",
+});
---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,90 @@
+import { useEffect, useRef } from "react";
+import api from "../../api/axiosConfig";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
+import "./Reviews.css";
+
+import React from "react";
+
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+  const revText = useRef();
+  let params = useParams();
+  const movieId = params.movieId;
+
+  useEffect(() => {
+    getMovieData(movieId);
+  }, []);
+
+  const addReview = async (e) => {
+    e.preventDefault();
+
+    const rev = revText.current;
+
+    try {
+      const response = await api.post("/api/v1/reviews", {
+        reviewBody: rev.value,
+        imdbId: movieId,
+      });
+
+      const updatedReviews = [...reviews, { body: rev.value }];
+
+      rev.value = "";
+
+      setReviews(updatedReviews);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Container className="componente">
+      <Row>
+        <Col>
+          <h3>Reseñas</h3>
+        </Col>
+      </Row>
+      <Row className="mt-2 contenedor">
+        <Col className="imagen-contenedor">
+          <img className="imagen" src={movie?.poster} alt="" />
+        </Col>
+        <Col>
+          {
+            <>
+              <Row>
+                <Col>
+                  <ReviewForm
+                    handleSubmit={addReview}
+                    revText={revText}
+                    labelText="Escribe una reseña"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <hr />
+                </Col>
+              </Row>
+            </>
+          }
+          {reviews?.map((r) => {
+            return (
+              <>
+                <Row>
+                  <Col>{r.body}</Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <hr />
+                  </Col>
+                </Row>
+              </>
+            );
+          })}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Reviews;
